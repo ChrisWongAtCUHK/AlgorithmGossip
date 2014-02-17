034fb2c5f92721f581115cb787e47bfe9a2551c4
Chris Wong <wc38.wong@gmail.com>
2014-02-17 17:52:06 +0800

    Ruby

---
+++ VeryOld/Hanoi.rb
@@ -0,0 +1,13 @@
+﻿# encoding: utf-8
+def hanoi(n, a, b, c)
+    if n == 1
+        [{from: a, to: c}]
+    else
+        hanoi(n - 1, a, c, b) + hanoi(1, a, b, c) + hanoi(n - 1, b, a, c)
+    end
+end
+
+print "請輸入整數："
+hanoi(gets.to_i, "A", "B", "C").each do |move|
+    print "盤子從 #{move[:from]} 移動至 #{move[:to]}\n"
+end
