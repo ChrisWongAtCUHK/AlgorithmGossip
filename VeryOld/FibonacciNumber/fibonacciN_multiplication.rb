47b5e46507396310405ee58570732e5cf1f74e36
Chris Wong <wc38.wong@gmail.com>
2014-02-20 09:52:30 +0800

    Fibonacci number calculation by multiplication

---
+++ VeryOld/FibonacciNumber/fibonacciN_multiplication.rb
@@ -0,0 +1,26 @@
+﻿# encoding: utf-8
+def fib(n)
+	if n <= 1
+		return n
+	end
+
+	if n == 2
+		return 1
+	else
+		i = n/2
+		f1 = fib(i + 1)
+		f2 = fib(i)
+		if (n % 2) == 0
+			return (f2 * (2 * f1 - f2))
+		else
+			return ((f1 * f1) + (f2 * f2))
+		end
+	end
+	return n
+end
+
+print "輸入個數："
+length = gets.to_i
+0.upto(length - 1) do |i|
+    print fib(i).to_s + ' '
+end
